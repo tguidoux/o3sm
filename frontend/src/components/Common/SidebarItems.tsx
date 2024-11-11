1ca331767fa123b0879a10e0934da3c4b1affca0
Theo Guidoux <theoguidoux@gmail.com>
2024-11-11 11:39:14 +0100

    ✨ Update SidebarItems component to swap icons for Parameters and User Settings for improved clarity

--- frontend/src/components/Common/SidebarItems.tsx
+++ frontend/src/components/Common/SidebarItems.tsx
@@ -1,15 +1,15 @@
 import { Box, Flex, Icon, Text, useColorModeValue } from "@chakra-ui/react"
 import { useQueryClient } from "@tanstack/react-query"
 import { Link } from "@tanstack/react-router"
-import { FiBriefcase, FiHome, FiSettings, FiUsers, FiKey } from "react-icons/fi"
+import { FiBriefcase, FiHome, FiSettings, FiUsers, FiUser } from "react-icons/fi"
 
 import type { UserPublic } from "../../client"
 
 const items = [
   { icon: FiHome, title: "Dashboard", path: "/" },
   { icon: FiBriefcase, title: "Credentials", path: "/credentials" },
-  { icon: FiKey, title: "Parameters", path: "/parameters" },
-  { icon: FiSettings, title: "User Settings", path: "/settings" },
+  { icon: FiSettings, title: "Parameters", path: "/parameters" },
+  { icon: FiUser, title: "User Settings", path: "/settings" },
 ]
 
 interface SidebarItemsProps {
